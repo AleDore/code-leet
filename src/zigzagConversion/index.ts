719253bdb2fb57363d3096288bbe326d5d375de8
AleDore <alessio85.dore@gmail.com>
2023-08-11 12:19:41 +0200

    fix algorithm

--- src/zigzagConversion/index.ts
+++ src/zigzagConversion/index.ts
@@ -1,21 +1,27 @@
 export function convert(s: string, numRows: number): string {
   const strArr = s.split("");
   var converted = "";
-  var delta = 0;
-  for (var i = 0; i < numRows; i++) {
-    delta = i < numRows / 2 ? numRows - i : i;
-    console.log(`delta=${delta}`);
-    for (var j = i; j < strArr.length; j = j + delta + 1) {
-      console.log(`i=${i}, j=${j}`);
-      converted += strArr[j];
+  var mod;
+  var tmpArr;
+  const acc = [strArr.slice(0, numRows)];
+  var charArrCount = numRows + 1;
+  for (var i = numRows; i < strArr.length; ) {
+    mod = (charArrCount + 1) % numRows;
+    if (mod > 0) {
+      tmpArr = new Array(numRows);
+      tmpArr[numRows - mod] = strArr[i];
+      acc.push(tmpArr);
+      charArrCount++;
+      i++;
+    } else {
+      acc.push(strArr.slice(i, i + numRows));
+      charArrCount += numRows + 2;
+      i += numRows;
     }
+  }
+
+  for (var idx = 0; idx < numRows; idx++) {
+    acc.forEach((row) => (converted += row[idx] ?? ""));
   }
   return converted;
 }
-
-// 0 1 2 3 4 5 6 7 8 9 10 11 12 13
-// P A Y P A L I S H I R  I  N  G
-
-// P   A   H   N
-// A P L S I I G
-// Y   I   R
--- src/zigzagConversion/tests/index.test.ts
+++ src/zigzagConversion/tests/index.test.ts
@@ -3,8 +3,9 @@ import { convert } from "..";
 describe("Zigzag conversion", () => {
   it.each([
     { s: "PAYPALISHIRING", numRows: 3, expectedOutput: "PAHNAPLSIIGYIR" },
+    { s: "PAYPALISHIRING", numRows: 4, expectedOutput: "PINALSIGYAHRPI" },
   ])(
-    "should return correctly a zigzag conversion string given the input $s",
+    "should return correctly a zigzag conversion string given the input $s and rows $numRows",
     ({ s, numRows, expectedOutput }) => {
       const res = convert(s, numRows);
       expect(res).toEqual(expectedOutput);
